36cd5c52b8b59848c27ea989a6428d9b01c0ffe3
Diego Pinheiro <diego.olliveirap@gmail.com>
2022-11-18 18:45:19 -0300

    feature: use redirect and session storage

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 import os
 from flask_moment import Moment
 from flask_bootstrap import Bootstrap
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, session, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -25,12 +25,11 @@ def internal_server_error(e):
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ""
-    return render_template("index.html", form=form, name=name)
+        session["name"] = form.name.data
+        return redirect(url_for("index"))
+    return render_template("index.html", form=form, name=session.get("name"))
 
 
 @app.route("/user/<name>")
