890b7da8a92229b0748b399593986f4969616610
Diego Pinheiro <diego.olliveirap@gmail.com>
2022-11-18 16:25:02 -0300

    feat: add NameForm using flask-wtf

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+import os
 from flask_moment import Moment
-from datetime import datetime
+from flask_bootstrap import Bootstrap
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 Bootstrap(app)
 Moment(app)
@@ -19,11 +23,21 @@ def internal_server_error(e):
     return render_template("error.html", message="Server Error"), 500
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html", current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+    return render_template("index.html", form=form, name=name)
 
 
 @app.route("/user/<name>")
 def user(name):
     return render_template("user.html", name=name)
+
+
+class NameForm(FlaskForm):
+    name = StringField("What is you name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
