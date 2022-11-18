1a5928ad737b5a261a0cacb79b89fb5e1e5c8eae
Diego Pinheiro <diego.olliveirap@gmail.com>
2022-11-18 11:30:31 -0300

    feat: add templates

---
+++ hello.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/user/<name>")
+def user(name):
+    return render_template("user.html", name=name)
