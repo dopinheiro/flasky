faec1315e8c5fd72149f846a1a7e42fb1d3f4c91
Diego Pinheiro <diego.olliveirap@gmail.com>
2022-11-18 12:09:44 -0300

    feature: add error routes and templates

--- hello.py
+++ hello.py
@@ -5,6 +5,16 @@ app = Flask(__name__)
 Bootstrap(app)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("error.html", message="Not Found"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("error.html", message="Server Error"), 500
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
